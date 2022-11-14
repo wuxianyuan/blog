bd170b637d44fd4f618837e094c43c8205946086
wuxianyuan <1047772876@qq.com>
2022-11-14 16:55:31 +0800

    feat: 路径配置

--- config/config.ts
+++ config/config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   antd: {},
   access: {},
   model: {},
+  publicPath: '/src/',
+  base: '/',
   initialState: {},
   request: {},
   layout: {
@@ -11,5 +13,9 @@ export default defineConfig({
   },
   routes,
   npmClient: "pnpm",
+  apiRoute: {
+    platform: 'vercel'
+  },
+  // plugins: [require.resolve('@umijs/plugins/dist/tailwindcss')],
   tailwindcss: {},
 });
--- config/routes/index.ts
+++ config/routes/index.ts
@@ -3,22 +3,22 @@ export default [
         path: "/",
         redirect: "/home",
     },
-
     {
         name: "首页",
         path: "/home",
-        component: "./Home",
+        component: "@/pages/Home",
     },
-
     {
         name: "权限演示",
         path: "/access",
-        component: "./Access",
+        component: "@/pages/Access",
     },
-
     {
         name: " CRUD 示例",
         path: "/table",
-        component: "./Table",
+        component: "@/pages/Table",
     },
+    { name: '新增', path: '/posts/create', component: '@/pages/Home' },
+    { name: '登录', path: '/login', component: '@/pages/Home' },
+    { name: '查看', path: '/posts/:postId', component: '@/pages/Home' },
 ]
